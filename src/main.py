460ddd7047857f6df72b68657eee3eb1946b3472
mojonapower <maca95@gmail.com>
2022-09-22 00:10:23 +0000

    last commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Fav_people
 #from models import Person
 
 app = Flask(__name__)
@@ -42,11 +42,51 @@ def handle_hello():
 
 @app.route("/people", methods=['GET'])
 def get_people():
-    return "all people"
+    # todos los personajes desde la base de datos
+    all_people = People.query.all() #Select * from People
+    print(all_people) #el resultado es un arreglo de clases
+    # people_serialized = []
+    # for people in all_people:
+    #     people_serialized.append(people.serialize())
+    # return jsonify(people_serialized)
+    all_people = list( map(lambda people:people.serialize(), all_people))
+    return jsonify(all_people)
+
+#busqueda por parametro
+# @app.route("/people/<people_name>", methods=['GET'])
+# def one_people(people_name):
+#     one = People.query.filter_by(name=people_name).first()
+#     return jsonify(one.serialize())
 
 @app.route("/people/<int:people_id>", methods=['GET'])
-def one_people():
-    return "just one people"
+def one_people(people_id):
+    one = People.query.get(people_id) #busqueda solo por el pk
+    return jsonify(one.serialize())
+   
+@app.route("/people/favorite/<int:people_id>", methods=['POST'])
+def add_people_fav(people_id):
+    one = People.query.get(people_id) #busqueda solo por el pk
+    user = User.query.get(1)
+    if(one):
+        new_fav = Fav_people()
+        new_fav.email = user.email
+        new_fav.people_id = people_id
+        db.session.add(new_fav)
+        db.session.commit()
+        return "Hecho!"
+    else:
+        raise APIException("no existe el personaje", status_code=404)
+
+@app.route("/people/favorite/<int:people_id>", methods=['DELETE'])
+def delete_people_fav(people_id):
+    one = Fav_people.query.filter_by(people_id=people_id).first()
+    if(one):
+        db.session.delete(one)
+        db.session.commit()
+        return "eliminado"
+    else:
+        raise APIException("no existe el personaje", status_code=404)
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
